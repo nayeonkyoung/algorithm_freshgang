f79a54ada509d0758264baa008602acae0b2510d
yeonkyoung na <42836692+yeongang@users.noreply.github.com>
2020-07-27 23:01:02 +0900

    완주하지 못한 선수
    완주 못할 뻔 했다

---
+++ 완주하지 못한 선수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String,Integer> map = new HashMap<>();
+        
+        for(String s : participant){
+            map.put(s, map.getOrDefault(s, 0) + 1); // key 값이 없는 경우 key값과 함께 1, 있는 경우 기존 1 + 1
+        }
+        
+        for(String s : completion){
+            map.put(s, map.get(s) - 1); // map key가 같을 경우 value -1 해서 0,1로 바뀜
+        }
+        
+        for(String key : map.keySet()){
+            if(map.get(key)!=0) return key; // map value가 1이면 완주를 못했다는 뜻
+        }
+        
+        return null;
+    }
+}
