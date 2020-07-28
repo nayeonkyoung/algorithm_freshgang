e268e020841ec9035024e4f4bf07b9b2b3ba222d
yeonkyoung na <42836692+yeongang@users.noreply.github.com>
2020-07-28 10:17:16 +0900

    수정사항 update

--- 완주하지 못한 선수.java
+++ 완주하지 못한 선수.java
@@ -1,15 +1,16 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String,Integer> map = new HashMap<>();
         
         for(String s : participant){
-            map.put(s, map.getOrDefault(s, 0) + 1); // key 값이 없는 경우 key값과 함께 1, 있는 경우 기존 1 + 1
+            map.put(s, map.getOrDefault(s, 0) + 1); // key 값이 없는 경우 key값과 함께 1, 있는 경우 기존값에 + 1 연산
         }
         
         for(String s : completion){
-            map.put(s, map.get(s) - 1); // map key가 같을 경우 value -1 해서 0,1로 바뀜
+            map.put(s, map.get(s) - 1); // map key가 같을 경우 value - 1 연산
         }
         
         for(String key : map.keySet()){
