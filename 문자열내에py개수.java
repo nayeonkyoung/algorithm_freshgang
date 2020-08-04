1169a1028001fd1ec6a728867dd835a39b14dd21
yeonkyoung na <42836692+yeongang@users.noreply.github.com>
2020-08-04 10:19:39 +0900

    코드 수정~

--- 문자열내에py개수.java
+++ 문자열내에py개수.java
@@ -5,7 +5,7 @@ class Solution {
         int pCount = 0;
         int yCount = 0;
         
-        for(int i = 0 ; i < s.length() ; i++){
+        for(int i = 0; i < s.length(); i++){
             if(Character.toUpperCase(s.charAt(i)) == 'P'){
                 pCount++; // 대소문자 구문 없이 P 여부 카운트
             }else if(Character.toUpperCase(s.charAt(i)) == 'Y'){
